34f2f517ad6296aa0a267c035a1266a2b2d4f801
ballkm <ball.kamol@gmail.com>
2020-02-09 16:09:32 +0700

    firstcommit HP

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#Create A blog models
+#title
+#pub_date
+#body
+#image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    iamge = models.ImageField(upload_to='images/')
+
+
+# Add the Blog app to the setting
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
